352b2db34dd88a4aa1a76cb98734822f9c3f529a
Rushan Khan <118595180+rushaan7@users.noreply.github.com>
2025-02-18 03:18:26 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Novasphere
+        </p>
+
+        <AnimatedTitle
+          title="Transforming <b>Visions </b>into  <br />Digital <b>R</b>eality"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Premium Design &#10073; Full-Service Expertise &#10073; Future-Ready Solutions &#10073; Trusted Reputation</p>
+          <p className="text-gray-500">
+            Award-worthy UI/UX that engages and converts.
+            dApps, AI, and scalable software.
+            A portfolio of satisfied global clients.
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
